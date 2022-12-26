ca2abc6e9acd8482e5c965fe4ebee0504cb77123
Roland Hobaj <rolandhobaj@gmail.com>
2022-12-26 14:24:31 +0100

    Creating two column, adding shadow

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import { fonts } from 'react-native-elements/dist/config';
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22,
-   backgroundColor : '#b9def0'
+    flex: 1,
+    paddingTop: 10,
+    backgroundColor: '#b9def0'
   },
   item: {
-    flex : 1,
-    padding: 10,
+    flex: 1,
+    padding: 5,
     fontSize: 18,
   },
 });
@@ -21,18 +21,42 @@ const FlatListBasics = () => {
     <View style={styles.container}>
       <FlatList
         data={[
-          {key: 'Backed Potato2'},
-          {key: 'Sushi'},
-          {key: 'etel'},
-          {key: 'etel2'}]}
-        renderItem={({item}) => 
-        <Card containerStyle={{backgroundColor: '#f4faf0'}}>
-          <Image source={require('./Images/B.jpg')} style={{ width: null, height: 250 }}/>
-          <Text style={{margin: 10, fontSize: 22, fontWeight: 'bold'}}>
-            {item.key}
-          </Text>
-        </Card>   
-      }
+          { key: 'Backed Potato2' },
+          { key: 'Sushi' },
+          { key: 'etel' },
+          { key: 'etel2' },
+          { key: 'etel3' }]}
+        renderItem={({ item }) =>
+          <View style={{ flexDirection: 'row' }}>
+            <Card containerStyle={{ backgroundColor: '#f4faf0', borderRadius: 40, flex: 1, shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 12,
+},
+shadowOpacity: 0.4,
+shadowRadius: 16.00,
+
+elevation: 24 }}>
+              <Image source={require('./images/B.jpg')} style={{ width: null, height: 130, borderRadius: 40 }} />
+              <Text style={{ margin: 5, fontSize: 17, fontWeight: 'bold', color: '#5884cc' }}>
+                {item.key}
+              </Text>
+            </Card>
+            <Card containerStyle={{
+              backgroundColor: '#f4faf0', borderRadius: 40, flex: 1, shadowColor: "#000",
+              shadowOffset: {width: 0,height: 8,},
+              shadowOpacity: 0.46,
+              shadowRadius: 11.14,
+
+              elevation: 17,
+            }}>
+              <Image source={require('./images/B.jpg')} style={{ width: null, height: 130, borderRadius: 40 }} />
+              <Text style={{ margin: 5, fontSize: 17, fontWeight: 'bold', color: '#5884cc' }}>
+                {item.key}
+              </Text>
+            </Card>
+          </View>
+        }
       />
     </View>
   );
