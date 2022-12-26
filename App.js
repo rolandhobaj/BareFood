14844abf8ab6d098049fb964b3d28fcec5dd279b
Roland Hobaj <rolandhobaj@gmail.com>
2022-12-26 14:47:08 +0100

    extract FoodCard.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
-import { Card } from 'react-native-elements'
-import { fonts } from 'react-native-elements/dist/config';
+import { FlatList, StyleSheet, View} from 'react-native';
+import FoodCard from './src/FoodCard'
 
 const styles = StyleSheet.create({
   container: {
@@ -28,33 +27,8 @@ const FlatListBasics = () => {
           { key: 'etel3' }]}
         renderItem={({ item }) =>
           <View style={{ flexDirection: 'row' }}>
-            <Card containerStyle={{ backgroundColor: '#f4faf0', borderRadius: 40, flex: 1, shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 12,
-},
-shadowOpacity: 0.4,
-shadowRadius: 16.00,
-
-elevation: 24 }}>
-              <Image source={require('./images/B.jpg')} style={{ width: null, height: 130, borderRadius: 40 }} />
-              <Text style={{ margin: 5, fontSize: 17, fontWeight: 'bold', color: '#5884cc' }}>
-                {item.key}
-              </Text>
-            </Card>
-            <Card containerStyle={{
-              backgroundColor: '#f4faf0', borderRadius: 40, flex: 1, shadowColor: "#000",
-              shadowOffset: {width: 0,height: 8,},
-              shadowOpacity: 0.46,
-              shadowRadius: 11.14,
-
-              elevation: 17,
-            }}>
-              <Image source={require('./images/B.jpg')} style={{ width: null, height: 130, borderRadius: 40 }} />
-              <Text style={{ margin: 5, fontSize: 17, fontWeight: 'bold', color: '#5884cc' }}>
-                {item.key}
-              </Text>
-            </Card>
+            <FoodCard/>
+            <FoodCard/>
           </View>
         }
       />
---
+++ src/FoodCard.js
@@ -0,0 +1,41 @@
+import { React, Component } from 'react'
+import { Card } from 'react-native-elements'
+import { Text, Image } from 'react-native';
+import { StyleSheet} from 'react-native';
+
+export default class FoodCard extends Component {
+  render() {
+    return (
+      <Card containerStyle={styles.container}>
+        <Image source={require('../images/B.jpg')} style={styles.image} />
+        <Text style={styles.text}>
+          Hihi
+        </Text>
+      </Card>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f4faf0',
+    borderRadius: 40,
+    flex: 1,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 8, },
+    shadowOpacity: 0.46,
+    shadowRadius: 11.14,
+    elevation: 17
+  },
+  image: {
+    width: null, 
+    height: 130, 
+    borderRadius: 40
+  },
+  text: {
+    margin: 5, 
+    fontSize: 18, 
+    fontWeight: 'bold', 
+    color: '#5884cc'
+  }
+});
