5d48eeb321382e481296f92ed9f8e178691b92f9
Roland Hobaj <rolandhobaj@gmail.com>
2022-12-30 12:37:28 +0100

    Creating add button with icon

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, StyleSheet, View, ImageBackground} from 'react-native';
+import { TouchableOpacity, StyleSheet, View, ImageBackground, Text} from 'react-native';
 import FoodSearchBar from './src/View/FoodSearchBar'
 import FoodFlatList from './src/View/FoodFlatList'
+import { Icon } from '@rneui/themed';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,6 +12,15 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
   },
+  roundButton: {
+    width: 50,  
+    height: 50,   
+    borderRadius: 60,            
+    backgroundColor: 'rgba(72,110,115, 0.8)',                                    
+    position: 'absolute',                                          
+    bottom: 40,                                                    
+    right: 40,
+  }
 });
 
 const FlatListBasics = () => {
@@ -19,6 +29,11 @@ const FlatListBasics = () => {
       <ImageBackground source={require('./images/bg.png')} resizeMode="cover" style={styles.image}>
       <FoodSearchBar/>
       <FoodFlatList/>
+      <TouchableOpacity
+        style={styles.roundButton}>
+           <Icon name='add' color='white' size={40} containerStyle={{margin:5}}
+      />
+      </TouchableOpacity>
       </ImageBackground>
     </View>
   );
