5d579afc2923db8fce007312858ddb9a300e7ed5
Roland Hobaj <rolandhobaj@gmail.com>
2022-12-31 08:51:13 +0100

    Add simple modal

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet, View, ImageBackground, Text} from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, StyleSheet, View, ImageBackground, Text, Button } from 'react-native';
 import FoodSearchBar from './src/View/FoodSearchBar'
 import FoodFlatList from './src/View/FoodFlatList'
 import { Icon } from '@rneui/themed';
+import Modal from "react-native-modal";
 
 const styles = StyleSheet.create({
   container: {
@@ -24,15 +25,31 @@ const styles = StyleSheet.create({
 });
 
 const FlatListBasics = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require('./images/bg.png')} resizeMode="cover" style={styles.image}>
       <FoodSearchBar/>
       <FoodFlatList/>
       <TouchableOpacity
+        onPress={toggleModal}
         style={styles.roundButton}>
            <Icon name='add' color='white' size={40} containerStyle={{margin:5}}
       />
+
+      <Modal isVisible={isModalVisible}>
+        <View style={{ flex: 1 }}>
+          <Text>Hello!</Text>
+
+          <Button title="Hide modal" onPress={toggleModal} />
+        </View>
+      </Modal>
+
       </TouchableOpacity>
       </ImageBackground>
     </View>
