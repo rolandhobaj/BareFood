2f846923efa3cc69a957d72b7fa27298473c8ff3
Roland Hobaj <rolandhobaj@gmail.com>
2023-10-13 15:55:42 +0200

    Add SearchableDropdown

--- src/View/FoodSearchBar.js
+++ src/View/FoodSearchBar.js
@@ -1,64 +1,103 @@
-import { SearchBar } from 'react-native-elements';
 import React, { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { View } from 'react-native';
 import useStore from '../Model/Store'
-import CustomSearchBar from './CustomSearchBar'
+import SearchableDropdown from 'react-native-searchable-dropdown';
+
+var items = [
+  {
+    id: 0,
+    name: 'Minden',
+  },
+  {
+    id: 1,
+    name: 'Leves',
+  },
+  {
+    id: 2,
+    name: 'Főétel',
+  },
+  {
+    id: 3,
+    name: 'Regnyuzsi',
+  },
+  {
+    id: 4,
+    name: 'Köret',
+  },
+  {
+    id: 5,
+    name: 'Desszert',
+  },
+];
 
 export default function FoodSearchBar(){
-  const [search, setSearch] = useState(0);
+  const [search, setSearch] = useState("");
+  const [all, setAll] = useState(false);
   const modifyTag = useStore(s => s.modifyTag);
 
   const updateTag = function(tag){
-    setSearch(tag);
     modifyTag(tag);
+  }
+
+  if (search === "") {
+    if (all){
+      placeholderText = 'Minden';
+    } else{
+      placeholderText = 'Írj be ide valamit';
+    }
+  } else {
+    placeholderText = search;
   }
 
     return (
       <View>
-       <CustomSearchBar value={search} onChange={updateTag} />
-          <View style={styles.quickSearchContainer}>
-          <TouchableOpacity style={styles.quickSearch} onPress={() => updateTag("Leves")}>
-            <Text style={styles.innerText}>Leves</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.quickSearch} onPress={() => updateTag("Főétel")}>
-            <Text style={styles.innerText}>Főétel</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.quickSearch} onPress={() => updateTag("Köret")}>
-            <Text style={styles.innerText}>Köret</Text>
-          </TouchableOpacity>
-        </View>
+        <SearchableDropdown
+            onItemSelect={(item) => {
+              let searchedText = item.name;
+              let all = false;
+              if (item.id === 0){                
+                searchedText=""
+                all = true;
+              }
+              setAll(all);
+              setSearch(searchedText);
+              updateTag(searchedText);
+            }}
+            containerStyle={{ padding: 5, backgroundColor: 'rgba(18,57,6,0.35)', margin: 10 }}
+            itemStyle={{
+              padding: 10,
+              marginTop: 2,
+              backgroundColor: 'rgba(18,57,6,0.35)',
+              borderColor: '#bbb',
+              borderWidth: 1,
+              borderRadius: 5,
+            }}
+            itemTextStyle={{ color: 'white' }}
+            itemsContainerStyle={{ maxHeight: 100 }}
+            items={items}
+            resetValue={false}
+            textInputProps={
+              {
+                placeholder: placeholderText,
+                placeholderTextColor: 'white',
+                color: 'white',
+                underlineColorAndroid: "transparent",
+                style: {
+                    paddingLeft: 9,
+                    borderWidth: 1,
+                    borderColor: '#ccc',
+                    height: 40,
+                    borderRadius: 5,
+                },
+                onTextChange: text => updateTag(text)
+              }
+            }
+            listProps={
+              {
+                nestedScrollEnabled: true,
+              }
+            }
+        />
       </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'rgba(0,0,0,0.0)',
-      borderTopWidth: 0,
-      borderBottomColor: 0,
-      marginTop:5,
-    },
-    inputContainer: {
-      backgroundColor: 'rgba(18,57,6,0.35)'
-    },
-    iconStyle: {
-      color: '#ffffff',
-    },
-    quickSearchContainer: {
-      flexDirection: 'row',
-      marginLeft:8,
-      marginRight:8,
-      height:35
-    },
-    quickSearch: {
-      alignItems: "center",
-      backgroundColor: 'rgba(18,57,6,0.35)',
-      padding: 5,
-      margin:2,
-      borderRadius:360,
-      flex:3
-    },
-    innerText: {
-      color: 'white'
-    }
-  });
